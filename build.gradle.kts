42f614ac76807254dc48f40343921ac70d6252bb
nerdFoo <gmlebreck@gmail.com>
2026-05-14 15:56:28 -0500

    build: dist task packages plugin jar + resource pack zip

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,3 +8,21 @@ allprojects {
         maven("https://repo.papermc.io/repository/maven-public/")
     }
 }
+
+tasks.register<Zip>("packageResourcePack") {
+    archiveBaseName.set("fooWeapons-resourcepack")
+    archiveVersion.set(project.version.toString())
+    destinationDirectory.set(layout.buildDirectory.dir("dist"))
+    from("resourcepack") {
+        include("pack.mcmeta")
+        include("assets/**")
+    }
+}
+
+tasks.register("dist") {
+    dependsOn(":plugin:jar", "packageResourcePack")
+    doLast {
+        println("Plugin JAR: " + project(":plugin").layout.buildDirectory.file("libs/plugin-${project.version}.jar").get())
+        println("Resource pack: " + layout.buildDirectory.file("dist/fooWeapons-resourcepack-${project.version}.zip").get())
+    }
+}
