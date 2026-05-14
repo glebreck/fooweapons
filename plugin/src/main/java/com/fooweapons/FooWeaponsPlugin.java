74f41c9b303fcc85e0434df20a85572fab124e44
nerdFoo <gmlebreck@gmail.com>
2026-05-14 15:31:18 -0500

    feat: handle left-click fire for weapon items

--- plugin/src/main/java/com/fooweapons/FooWeaponsPlugin.java
+++ plugin/src/main/java/com/fooweapons/FooWeaponsPlugin.java
@@ -1,24 +1,39 @@
 package com.fooweapons;
 
+import com.fooweapons.fire.FireListener;
+import com.fooweapons.fire.FireService;
+import com.fooweapons.item.ItemState;
+import com.fooweapons.item.PdcKeys;
+import com.fooweapons.item.WeaponItemFactory;
 import com.fooweapons.weapon.WeaponRegistry;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class FooWeaponsPlugin extends JavaPlugin {
-    private final WeaponRegistry weapons = new WeaponRegistry();
+    private WeaponRegistry weapons;
+    private PdcKeys pdcKeys;
+    private WeaponItemFactory itemFactory;
+    private ItemState itemState;
 
     @Override
     public void onEnable() {
+        this.pdcKeys = new PdcKeys(this);
+        this.itemFactory = new WeaponItemFactory(pdcKeys);
+        this.itemState = new ItemState(pdcKeys);
+        this.weapons = new WeaponRegistry();
         try {
             weapons.loadFromClasspath(getClassLoader(), "weapons/pistol_01.yml");
-            getLogger().info("Loaded " + weapons.size() + " weapon(s).");
-            getLogger().info("fooWeapons enabled.");
         } catch (Exception e) {
             getLogger().severe("Failed to load weapons: " + e.getMessage());
             getServer().getPluginManager().disablePlugin(this);
+            return;
         }
+        FireService fireService = new FireService(itemState);
+        getServer().getPluginManager().registerEvents(
+            new FireListener(weapons, itemFactory, fireService), this);
+        getLogger().info("fooWeapons enabled with " + weapons.size() + " weapon(s).");
     }
 
-    public WeaponRegistry weapons() {
-        return weapons;
-    }
+    public WeaponRegistry weapons() { return weapons; }
+    public WeaponItemFactory itemFactory() { return itemFactory; }
+    public ItemState itemState() { return itemState; }
 }
---
+++ plugin/src/main/java/com/fooweapons/fire/FireListener.java
@@ -0,0 +1,38 @@
+package com.fooweapons.fire;
+
+import com.fooweapons.item.WeaponItemFactory;
+import com.fooweapons.weapon.Weapon;
+import com.fooweapons.weapon.WeaponRegistry;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.inventory.ItemStack;
+import java.util.Optional;
+
+public final class FireListener implements Listener {
+    private final WeaponRegistry registry;
+    private final WeaponItemFactory factory;
+    private final FireService fireService;
+
+    public FireListener(WeaponRegistry registry, WeaponItemFactory factory, FireService fireService) {
+        this.registry = registry;
+        this.factory = factory;
+        this.fireService = fireService;
+    }
+
+    @EventHandler
+    public void onInteract(PlayerInteractEvent event) {
+        if (event.getAction() != Action.LEFT_CLICK_AIR && event.getAction() != Action.LEFT_CLICK_BLOCK) return;
+        if (event.getHand() != null && event.getHand() != org.bukkit.inventory.EquipmentSlot.HAND) return;
+        Player player = event.getPlayer();
+        ItemStack stack = player.getInventory().getItemInMainHand();
+        if (!factory.isWeapon(stack)) return;
+        event.setCancelled(true);
+        String id = factory.getWeaponId(stack);
+        Optional<Weapon> weapon = registry.get(id);
+        if (weapon.isEmpty()) return;
+        fireService.tryFire(player, stack, weapon.get());
+    }
+}
