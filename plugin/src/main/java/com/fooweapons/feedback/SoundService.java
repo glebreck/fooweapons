e4bd5d23becae93ec64975ef47100f00425c282e
nerdFoo <gmlebreck@gmail.com>
2026-05-18 07:38:41 -0500

    feat: add click + denied-click sounds for fire-mode toggle

--- plugin/src/main/java/com/fooweapons/feedback/SoundService.java
+++ plugin/src/main/java/com/fooweapons/feedback/SoundService.java
@@ -17,6 +17,16 @@ public final class SoundService {
         play(player, soundId, 0.5f, 1.2f);
     }
 
+    /** Short positive click for a successful fire-mode toggle. */
+    public void playClick(Player player) {
+        play(player, "minecraft:ui.button.click", 0.6f, 1.4f);
+    }
+
+    /** Short negative click for an attempted toggle on a single-mode weapon. */
+    public void playDeniedClick(Player player) {
+        play(player, "minecraft:ui.button.click", 0.6f, 0.7f);
+    }
+
     private void play(Player player, String soundId, float volume, float pitch) {
         Location loc = player.getLocation();
         player.getWorld().playSound(loc, soundId, SoundCategory.PLAYERS, volume, pitch);
